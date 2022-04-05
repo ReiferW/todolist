e00985d98b9cdb1cfd858a1fca0c479668be969d
ReiferW <96327492+ReiferW@users.noreply.github.com>
2022-04-05 12:00:16 +0800

    Add js to ACTIVE button

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ let newTaskInput = document.getElementById("task-input")
 let todoListContainer = document.getElementById("todo-list")
 let templateElement = document.getElementById("list-item-template")
 let template = templateElement.innerHTML
+
+let showActiveButton = document.getElementById("show-active")
 // step 2: Write a function to implement the behaviour
 function onAddTaskClicked (event){
     let taskName = newTaskInput.value;
@@ -28,6 +30,20 @@ function onTodoListClicked(event){
         targetElement.classList.remove("completed");
     }
 }
+
+function showActiveTasks(){
+    let tasks = document.getElementsByClassName("task")
+    for (let i = 0;i<tasks.length;i++)
+    {
+        if (tasks[i].classList.contains("completed"))
+        {
+            tasks[i].style.display = "none"
+        }else{
+            tasks[i].style.display = "block"
+        }
+    }
+}
 // step 3: link element, function and event
 addTaskButton.addEventListener('click',onAddTaskClicked);
 todoListContainer.addEventListener('click',onTodoListClicked);
+showActiveButton.addEventListener('click',showActiveTasks);
